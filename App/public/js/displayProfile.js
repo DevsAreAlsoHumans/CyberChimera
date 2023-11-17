b65231343b9e4bd22763185aff3ce5f993160658
FlAvril <92847885+FCORMERAIS@users.noreply.github.com>
2023-11-17 12:27:59 +0100

    ajout fond d'écrans + correction bug securité

--- App/public/js/displayProfile.js
+++ App/public/js/displayProfile.js
@@ -1,13 +1,13 @@
 
-if (localStorage.getItem("name")!= ""){
-    document.getElementById("profil").style.display = "flex"
+if (localStorage.getItem("name")== "" || localStorage.getItem("name") == null){
+   document.getElementById("profil").style.display = "none"
  }else{
-    document.getElementById("profil").style.display = "none"
+   document.getElementById("profil").style.display = "flex"
  }
 
-if (localStorage.getItem("name")!= ""){
-   document.getElementById("interface").style.display = "flex"
-}else{
+if (localStorage.getItem("name")== "" || localStorage.getItem("name") == null){
    document.getElementById("interface").style.display = "none"
+}else{
+   document.getElementById("interface").style.display = "flex"
 }
  
